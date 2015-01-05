5de789b398fb854360e1823078276b8e6940f1c2
Ian Eslick <eslick@alum.mit.edu>
2015-01-04 23:21:56 -0800

    0.9.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.vitalreactor/probe "0.9.6-SNAPSHOT"
+(defproject com.vitalreactor/probe "0.9.5"
   :description "A library for interacting with dynamic program state"
   :url "http://github.com/vitalreactor/probe"
   :license {:name "MIT License" :url "http://opensource.org/licenses/MIT"}
